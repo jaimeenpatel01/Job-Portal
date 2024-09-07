64bd66527284486490e4ae2b1d5a57b4f854786a
Jaimeen Patel <jaimeenpatel2017@gmail.com>
2024-09-07 15:54:18 +0530

    Add new files or changes

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -5,7 +5,6 @@ import { Button } from './ui/button'
 import { Contact, Mail, Pen } from 'lucide-react'
 import { Badge } from './ui/badge'
 import { Label } from './ui/label'
-import AppliedJobTable from './AppliedJobTable'
 import UpdateProfileDialog from './UpdateProfileDialog'
 import { useSelector } from 'react-redux'
 import useGetAppliedJobs from '@/hooks/useGetAppliedJobs'
@@ -59,11 +58,7 @@ const Profile = () => {
                     }
                 </div>
             </div>
-            <div className='max-w-4xl mx-auto bg-white rounded-2xl'>
-                <h1 className='font-bold text-lg my-5'>Applied Jobs</h1>
-                {/* Applied Job Table   */}
-                <AppliedJobTable />
-            </div>
+          
             <UpdateProfileDialog open={open} setOpen={setOpen}/>
         </div>
     )
