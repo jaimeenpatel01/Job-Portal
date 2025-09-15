9a41aa8015df1f0fccfdd1351ba95f13c715ff92
Jaimeen Personal <jaimeenpatel2017@gmail.com>
2025-09-15 14:14:37 +0530

    catch block in jwt

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -20,6 +20,10 @@ const isAuthenticated = async (req, res, next) => {
         next();
     } catch (error) {
         console.log(error);
+        return res.status(401).json({
+            message: "Token verification failed",
+            success: false
+        });
     }
 }
 export default isAuthenticated;
