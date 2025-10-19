30f3690d1221ae731121558310b9b057b3fae7f2
Jaimeen Personal <jaimeenpatel2017@gmail.com>
2025-10-19 12:32:18 +0530

    Fix company logo

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -81,9 +81,14 @@ export const getAllJobs = async (req, res) => {
 export const getJobById = async (req, res) => {
     try {
         const jobId = req.params.id;
-        const job = await Job.findById(jobId).populate({
-            path:"applications"
-        });
+        const job = await Job.findById(jobId).populate([
+            {
+                path: "applications"
+            },
+            {
+                path: "company"
+            }
+        ]);
         
         if (!job) {
             return res.status(404).json({
